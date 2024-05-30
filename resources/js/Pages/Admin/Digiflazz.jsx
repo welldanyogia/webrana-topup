8a2ff2bb8ef581b066c7448cab51334846f0e7cb
welldanyogia <welldanyogia01@gmail.com>
2024-05-30 20:30:07 +0700

    update

--- resources/js/Pages/Admin/Digiflazz.jsx
+++ resources/js/Pages/Admin/Digiflazz.jsx
@@ -178,7 +178,7 @@ export default function Digiflazz(props) {
                             <div className="sm:col-span-9">
                                 <div className="sm:flex">
                                     <input id="username_digiflazz" type="text" onChange={handleChange}
-                                           value={digi_auth.username}
+                                           value={values.username_digiflazz}
                                            className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm -mt-px -ms-px first:rounded-t-lg last:rounded-b-lg sm:first:rounded-s-lg sm:mt-0 sm:first:ms-0 sm:first:rounded-se-none sm:last:rounded-es-none sm:last:rounded-e-lg text-sm relative focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
                                            placeholder="Username Digiflazz"/>
                                 </div>
