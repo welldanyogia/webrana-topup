9d3aff8580d2057d2d5eaeb5f423317f964a5f3c
welldanyogia <welldanyogia01@gmail.com>
2024-05-30 22:18:42 +0700

    update

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -25,19 +25,16 @@ export default function Navbar() {
 
                 {/*// <!-- Button Group */}
                 <div className="flex items-center gap-x-2 ms-auto py-1 md:ps-6 md:order-3 md:col-span-3">
+                    <div className='sm:hidden'>
                     <button
                         type="button" className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-xl border border-gray-200 text-black hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:hover:bg-white/10 dark:text-white dark:hover:text-white" data-hs-overlay="#hs-modal-signin">
-                        {/*<Link*/}
-                        {/*    href={route('login')}*/}
-                        {/*    className="rounded-md text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"*/}
-                        {/*>*/}
                             Sign in
-                        {/*</Link>*/}
                     </button>
                     <button
                         type="submit" className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-xl border border-transparent bg-lime-400 text-black hover:bg-lime-500 transition disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-lime-500">
                         Sign up
                     </button>
+                    </div>
 
                     <div className="md:hidden">
                         <button type="button" className="hs-collapse-toggle size-[38px] flex justify-center items-center text-sm font-semibold rounded-xl border border-gray-200 text-black hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-neutral-700 dark:hover:bg-neutral-700" data-hs-collapse="#navbar-collapse-with-animation" aria-controls="navbar-collapse-with-animation" aria-label="Toggle navigation">
@@ -50,15 +47,32 @@ export default function Navbar() {
 
                 {/*<!-- Collapse */}
                 <div id="navbar-collapse-with-animation" className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block md:w-auto md:basis-auto md:order-2 md:col-span-6">
-                    <div className="flex flex-col gap-y-4 gap-x-0 mt-5 md:flex-row md:justify-center md:items-center md:gap-y-0 md:gap-x-7 md:mt-0">
+                    <div
+                        className="flex flex-col gap-y-4 gap-x-0 mt-5 md:flex-row md:justify-center md:items-center md:gap-y-0 md:gap-x-7 md:mt-0">
                         <div>
-                            <a className="relative inline-block text-black before:absolute before:bottom-0.5 before:start-0 before:-z-[1] before:w-full before:h-1 before:bg-lime-400 dark:text-white" href="/" aria-current="page">Beranda</a>
+                            <a className="relative inline-block text-black before:absolute before:bottom-0.5 before:start-0 before:-z-[1] before:w-full before:h-1 before:bg-lime-400 dark:text-white"
+                               href="/" aria-current="page">Beranda</a>
                         </div>
                         <div>
-                            <a className="inline-block text-black hover:text-gray-600 dark:text-white dark:hover:text-neutral-300" href="/transactions/history">Riwayat Pesanan</a>
+                            <a className="inline-block text-black hover:text-gray-600 dark:text-white dark:hover:text-neutral-300"
+                               href="/transactions/history">Riwayat Pesanan</a>
                         </div>
                         <div>
-                            <a className="inline-block text-black hover:text-gray-600 dark:text-white dark:hover:text-neutral-300" href="#">Tentang</a>
+                            <a className="inline-block text-black hover:text-gray-600 dark:text-white dark:hover:text-neutral-300"
+                               href="#">Tentang</a>
+                        </div>
+                        <div className='flex'>
+                            <button
+                                type="button"
+                                className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-xl border border-gray-200 text-black hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:hover:bg-white/10 dark:text-white dark:hover:text-white"
+                                data-hs-overlay="#hs-modal-signin">
+                                Sign in
+                            </button>
+                            <button
+                                type="submit"
+                                className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-xl border border-transparent bg-lime-400 text-black hover:bg-lime-500 transition disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-lime-500">
+                                Sign up
+                            </button>
                         </div>
                     </div>
                 </div>
