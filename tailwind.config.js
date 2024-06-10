3c702f83ce253af3d793783ca0d90089c4b07473
welldanyogia <welldanyogia01@gmail.com>
2024-06-11 02:29:50 +0700

    juju

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,8 +20,10 @@ export default {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
             colors: {
-                'primary' : colors.gray,
-                'primary-dark' : colors.gray,
+                // 'primary' : colors.gray,
+                // 'primary-dark' : colors.gray,
+                'primary' : colors.blue,
+                'primary-dark' : colors.blue,
                 'secondary' : colors.lime,
                 'blue': {
                     '50': '#e8efff',
