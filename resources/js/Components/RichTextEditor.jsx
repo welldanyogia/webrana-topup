ae2cb6b29b063dc4820bdecdd55ddd4c99f36d4f
welldanyogia <welldanyogia01@gmail.com>
2024-06-17 23:26:13 +0700

    fixing brand_desc

--- resources/js/Components/RichTextEditor.jsx
+++ resources/js/Components/RichTextEditor.jsx
@@ -1,23 +1,23 @@
-import 'froala-editor/css/froala_style.min.css';
-import 'froala-editor/css/froala_editor.pkgd.min.css';
-import FroalaEditorComponent from 'react-froala-wysiwyg';
-import 'froala-editor/js/plugins.pkgd.min.js';
-import {useState} from "react";
-
-export default function RichTextEditor ({handleModelChange,model}){
-
-    return (
-
-        <div className="editor">
-
-            <h3> Description</h3>
-
-            <FroalaEditorComponent tag='textarea' onModelChange={handleModelChange} model={model}/>
-
-        </div>
-
-    );
-
-}
-
-
+// import 'froala-editor/css/froala_style.min.css';
+// import 'froala-editor/css/froala_editor.pkgd.min.css';
+// import FroalaEditorComponent from 'react-froala-wysiwyg';
+// import 'froala-editor/js/plugins.pkgd.min.js';
+// import {useState} from "react";
+//
+// export default function RichTextEditor ({handleModelChange,model}){
+//
+//     return (
+//
+//         <div className="editor">
+//
+//             <h3> Description</h3>
+//
+//             <FroalaEditorComponent tag='textarea' onModelChange={handleModelChange} model={model}/>
+//
+//         </div>
+//
+//     );
+//
+// }
+//
+//
