b665f0d95381c546e1ba08cfb1721d5f7a46f6f6
welldanyogia <welldanyogia01@gmail.com>
2024-06-17 21:37:46 +0700

    add bank tf

--- resources/js/Pages/Admin/BankAccount.jsx
+++ resources/js/Pages/Admin/BankAccount.jsx
@@ -163,7 +163,7 @@ export default function BankAccount(props) {
                                            value={values.api_key}
                                            disabled={values.status === 0}
                                            className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm -mt-px -ms-px first:rounded-t-lg last:rounded-b-lg sm:first:rounded-s-lg sm:mt-0 sm:first:ms-0 sm:first:rounded-se-none sm:last:rounded-es-none sm:last:rounded-e-lg text-sm relative focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-                                           placeholder="API Key MutasiBank"/>
+                                           placeholder="Personal Token Moota App"/>
                                 </div>
                             </div>
                         </div>
