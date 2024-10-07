9bd725d4fc9dba271e3f52b1a4c85d7d2abe223d
welldanyogia <welldanyogia01@gmail.com>
2024-10-08 03:40:44 +0700

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.jsx',
-        // './node_modules/preline/preline.js',
+        './node_modules/preline/preline.js',
         './node_modules/preline/dist/*.js',
         "node_modules/flowbite/**/*.js",
     ],
