a17ea5ccb68aab4e1fac51f8e1cbe4900008f8c1
welldanyogia <welldanyogia01@gmail.com>
2024-10-08 04:01:19 +0700

    update tailwind.config.js

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -206,6 +206,6 @@
             });
         </script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
-        <script src="node_modules/preline/dist/preline.js"></script>
+{{--        <script src="node_modules/preline/dist/preline.js"></script>--}}
     </body>
 </html>
